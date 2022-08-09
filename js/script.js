cfc2f64592a7aa04c6f7a41fd74b1f5bd6461668
unknown <jennywest715@yahoo.com>
2022-08-08 20:36:43 -0700

    take home practicing logs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
+// console.log("This is a message from console")
 
+/*console.log("Can make more than one message!")
+console.log("Can make more than one message!")*/
 
-console. log("This is a message from console")
+console.log("SOUPHANY JENNY WEST!!!")
 
-console. log("Can make more than one message!")
+console.log("My favorite food is Alfredo ALL PASTA!")
